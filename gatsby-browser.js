c33a651f5078d80c953ceeefad5830152d29bd18
Suttisak Denduangchai <suttisak.lukesp@Outlook.com>
2019-10-22 10:53:52 +0700

    Remove entrie text from compentencies.mdx and replace with abstract

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -7,3 +7,14 @@
 // You can delete this file if you're not using it
 import "./src/styles/layout.css";
 import "./src/styles/global.css";
+
+export const onServiceWorkerUpdateReady = () => {
+    const answer = window.confirm(
+        `เว็บไซต์นี้มีการอัพเดต ` +
+        `ต้องรีเฟรสหน้านี้ใหม่ เพื่อแสดงรุ่นล่าสุด?`
+    );
+
+    if (answer === true) {
+        window.location.reload();
+    }
+}
