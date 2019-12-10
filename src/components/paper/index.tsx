8a5346842620c88d5d4ba29a88d61bd845a3a4d0
Suttisak Denduangchai <Suttisak.lukesp@outlook.com>
2019-12-10 11:31:09 +0700

    gi[+] IntersectionObserver API

--- src/components/paper/index.tsx
+++ src/components/paper/index.tsx
@@ -1,33 +1,77 @@
 import * as React from "react";
 import * as PropTypes from "prop-types";
 import styles from "./paper.module.scss";
+import { InView } from "react-intersection-observer";
 
-class Paper extends React.Component<{ wide: any, side: any, transparent: any, sbs: any, children: any, style: any }, {}>{
-    constructor(props) {
-        super(props);
-    }
+class Paper extends React.Component<
+  {
+    wide: any;
+    side: any;
+    transparent: any;
+    sbs: any;
+    children: any;
+    style: any;
+  },
+  {}
+> {
+  constructor(props) {
+    super(props);
+  }
 
-    public static propTypes = {
-        children: PropTypes.node.isRequired
-    };
+  public static propTypes = {
+    children: PropTypes.node.isRequired
+  };
 
-    render() {
-        const { wide, side, transparent, sbs, children, style , ...othersProps} = this.props;
-        const classname = `${transparent ? styles.trans : ``} ${side ? `` : styles.noAside}`;
-        const containerClassName = `${styles.articleContainer} ${wide ? styles.wide : ``} `;
-        const innerContainerClassName = `${styles.innerArticleContainer} ${sbs ? styles.sbs : ``}`;
-        return (
-            <div className={containerClassName} style={style} {...othersProps}>
-                <div className={innerContainerClassName}>
-                    {side ? (<aside>{side}</aside>) : null}
-                    <article style={style} className={classname}>
-                        {children}
-                    </article>
-                </div>
+  private hasAlreadyShown = false;
+
+  render() {
+    const {
+      wide,
+      side,
+      transparent,
+      sbs,
+      children,
+      style,
+      ...othersProps
+    } = this.props;
+
+    const classname = `${transparent ? styles.trans : ``} ${
+      side ? `` : styles.noAside
+    }`;
+    const containerClassName = `${styles.articleContainer} ${
+      wide ? styles.wide : ``
+    } `;
+    const innerContainerClassName = `${styles.innerArticleContainer} ${
+      sbs ? styles.sbs : ``
+    }`;
+
+    return (
+      <InView>
+        {({ inView, ref, entry }) => {
+            this.hasAlreadyShown = inView || this.hasAlreadyShown;
+          return (
+            <div
+              ref={ref}
+              className={
+                containerClassName +
+                " " +
+                (this.hasAlreadyShown || inView ? styles.show : styles.hide)
+              }
+              style={style}
+              {...othersProps}
+            >
+              <div className={innerContainerClassName}>
+                {side ? <aside>{side}</aside> : null}
+                <article style={style} className={classname}>
+                  {children}
+                </article>
+              </div>
             </div>
-        );
-    }
-
-};
+          );
+        }}
+      </InView>
+    );
+  }
+}
 
 export default Paper;
