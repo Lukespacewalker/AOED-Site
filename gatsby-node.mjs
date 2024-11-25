e93b29355d7516c05252862f89fc3272238c2a72
Suttisak Denduangchai <Suttisak.lukesp@outlook.com>
2024-11-25 11:59:47 +0700

    Refactor gatsby-node.mjs for improved readability and update schema customization for authors

--- gatsby-node.mjs
+++ gatsby-node.mjs
@@ -6,89 +6,95 @@
 
 import { resolve, dirname } from "path";
 import { createFilePath } from "gatsby-source-filesystem";
-import { fileURLToPath } from "url"
-const __dirname = dirname(fileURLToPath(import.meta.url))
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export function onCreateWebpackConfig({ actions }) {
-    actions.setWebpackConfig({
-        resolve: {
-            alias: {
-                "@components": resolve(__dirname, "src/components"),
-                "@utilities": resolve(__dirname, "src/utilities"),
-                "@images": resolve(__dirname, "src/images"),
-                "@authors": resolve(__dirname, "src/contents/authors"),
-            }
-        }
-    });
+  actions.setWebpackConfig({
+    resolve: {
+      alias: {
+        "@components": resolve(__dirname, "src/components"),
+        "@utilities": resolve(__dirname, "src/utilities"),
+        "@images": resolve(__dirname, "src/images"),
+        "@authors": resolve(__dirname, "src/contents/authors"),
+      },
+    },
+  });
 }
 
 export function onCreateNode({ node, getNode, actions }) {
-    const { createNodeField } = actions;
-    if (node.internal.type === `Mdx`) {
-        const slug = createFilePath({ node, getNode, basePath: `pages` });
-        createNodeField({
-            node,
-            name: `slug`,
-            value: slug
-        });
-    }
+  const { createNodeField } = actions;
+  if (node.internal.type === `Mdx`) {
+    const slug = createFilePath({ node, getNode, basePath: `pages` });
+    createNodeField({
+      node,
+      name: `slug`,
+      value: slug,
+    });
+  }
 }
 
 export function createPages({ graphql, actions }) {
-    const { createPage } = actions;
-    return graphql(`
+  const { createPage } = actions;
+  return graphql(`
     {
       allMdx {
         edges {
           node {
+            id
             fields {
               slug
             }
             internal {
               contentFilePath
             }
-            frontmatter{
-                attachments {
-                    name
-                    publicURL
-                    childImageSharp {
-                        gatsbyImageData(layout: FULL_WIDTH)
-                        original {
-                        height
-                        width
-                        }
-                    }
+            frontmatter {
+              attachments {
+                name
+                publicURL
+                childImageSharp {
+                  gatsbyImageData(layout: FULL_WIDTH)
+                  original {
+                    height
+                    width
+                  }
                 }
+              }
             }
           }
         }
       }
     }
-  `).then(result => {
-        result.data.allMdx.edges.forEach(({ node }) => {
-            createPage({
-                path: node.fields.slug,
-                component: `${resolve(`./src/templates/information-page.tsx`)}?__contentFilePath=${node.internal.contentFilePath}`,
-                context: {
-                    // Data passed to context is available
-                    // in page queries as GraphQL variables.
-                    slug: node.fields.slug,
-                    images: node.frontmatter.attachments
-                }
-            });
-        });
+  `).then((result) => {
+    result.data.allMdx.edges.forEach(({ node }) => {
+      createPage({
+        path: node.fields.slug,
+        component: `${resolve(
+          `./src/templates/information-page.tsx`
+        )}?__contentFilePath=${node.internal.contentFilePath}`,
+        context: {
+          // Data passed to context is available
+          // in page queries as GraphQL variables.
+          slug: node.fields.slug,
+          images: node.frontmatter.attachments,
+        },
+      });
     });
+  });
 }
 
-
 export function createSchemaCustomization({ actions, schema }) {
-    const { createTypes } = actions;
-    const typeDefs = [
-        "type Mdx implements Node { frontmatter: MdxFrontmatter }",
-        `type MdxFrontmatter {
-            authors: [AuthorslistJson] @link(by: "id") 
-        }`
-        //date: Date @dateformat(formatString: "dddd, DD MMMM YYYY", locale: "th")
-    ];
-    createTypes(typeDefs);
+  const { createTypes } = actions;
+  const typeDefs = [
+    "type Mdx implements Node { frontmatter: MdxFrontmatter }",
+    `type MdxFrontmatter {
+            authors: [AuthorslistJson] @link(by: "unique") 
+        }
+        type AuthorslistJson implements Node {
+            posts: [Mdx] @link(by: "frontmatter.authors.unique", from: "unique") # easy back-ref
+        }    
+        `,
+    //date: Date @dateformat(formatString: "dddd, DD MMMM YYYY", locale: "th")
+  ];
+  createTypes(typeDefs);
 }
